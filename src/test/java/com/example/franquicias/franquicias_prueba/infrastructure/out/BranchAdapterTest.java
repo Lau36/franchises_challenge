bda125986bd423f23e5a92e219090cbc9fb94484
Laura Jaimes <laura.jaimes@correounivalle.edu.co>
2025-03-05 22:51:25 -0500

    unit test to adapters implemented

--- src/test/java/com/example/franquicias/franquicias_prueba/infrastructure/out/BranchAdapterTest.java
+++ src/test/java/com/example/franquicias/franquicias_prueba/infrastructure/out/BranchAdapterTest.java
@@ -1,8 +1,119 @@
 package com.example.franquicias.franquicias_prueba.infrastructure.out;
 
+import com.example.franquicias.franquicias_prueba.domain.models.Branch;
+import com.example.franquicias.franquicias_prueba.infrastructure.out.adapter.BranchAdapter;
+import com.example.franquicias.franquicias_prueba.infrastructure.out.entity.BranchEntity;
+import com.example.franquicias.franquicias_prueba.infrastructure.out.repository.IBranchRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BranchAdapterTest {
+    @Mock
+    private IBranchRepository branchRepository;
+
+    @InjectMocks
+    private BranchAdapter branchAdapter;
+
+    @Test
+    public void saveFranchise_shouldSaveBranchTest() {
+        Branch branch = new Branch(1L, "Test Branch", 2L);
+        BranchEntity branchEntity = BranchEntity.builder()
+                .name(branch.getName())
+                .franchiseId(branch.getFranchiseId())
+                .build();
+
+        when(branchRepository.save(any(BranchEntity.class))).thenReturn(Mono.just(branchEntity));
+
+        Mono<Void> result = branchAdapter.saveFranchise(branch);
+
+        StepVerifier.create(result).verifyComplete();
+
+        verify(branchRepository, times(1)).save(any(BranchEntity.class));
+    }
+
+    @Test
+    public void existsFranchise_shouldReturnTrueTest() {
+        when(branchRepository.existsBranchByName("Test Branch")).thenReturn(Mono.just(true));
+
+        Mono<Boolean> result = branchAdapter.existsFranchise("Test Branch");
+
+        StepVerifier.create(result)
+                .expectNext(true)
+                .verifyComplete();
+
+        verify(branchRepository, times(1)).existsBranchByName("Test Branch");
+    }
+
+    @Test
+    public void existsFranchise_shouldReturnFalseTest() {
+        when(branchRepository.existsBranchByName("Test Branch")).thenReturn(Mono.just(false));
+
+        Mono<Boolean> result = branchAdapter.existsFranchise("Test Branch");
+
+        StepVerifier.create(result)
+                .expectNext(false)
+                .verifyComplete();
+
+        verify(branchRepository, times(1)).existsBranchByName("Test Branch");
+    }
+
+    @Test
+    public void existsBranch_shouldReturnBranchTest() {
+        BranchEntity branchEntity = new BranchEntity(1L, "Test Branch", 2L);
+        when(branchRepository.findBranchByName("Test Branch")).thenReturn(Mono.just(branchEntity));
+
+        Mono<Branch> result = branchAdapter.existsBranch("Test Branch");
+
+        StepVerifier.create(result)
+                .expectNextMatches(branch -> branch.getId().equals(1L) && branch.getName().equals("Test Branch") && branch.getFranchiseId().equals(2L))
+                .verifyComplete();
+
+        verify(branchRepository, times(1)).findBranchByName("Test Branch");
+    }
+
+    @Test
+    public void existsBranch_shouldReturnEmptyTest() {
+        when(branchRepository.findBranchByName("Test Branch")).thenReturn(Mono.empty());
+
+        Mono<Branch> result = branchAdapter.existsBranch("Test Branch");
+
+        StepVerifier.create(result)
+                .verifyComplete();
+
+        verify(branchRepository, times(1)).findBranchByName("Test Branch");
+    }
+
+    @Test
+    public void existsBranchById_shouldReturnTrueTest() {
+        when(branchRepository.existsBranchById(1L)).thenReturn(Mono.just(true));
+
+        Mono<Boolean> result = branchAdapter.existsBranchById(1L);
+
+        StepVerifier.create(result)
+                .expectNext(true)
+                .verifyComplete();
+
+        verify(branchRepository, times(1)).existsBranchById(1L);
+    }
+
+    @Test
+    public void existsBranchById_shouldReturnFalseTest() {
+        when(branchRepository.existsBranchById(1L)).thenReturn(Mono.just(false));
+
+        Mono<Boolean> result = branchAdapter.existsBranchById(1L);
+
+        StepVerifier.create(result)
+                .expectNext(false)
+                .verifyComplete();
+
+        verify(branchRepository, times(1)).existsBranchById(1L);
+    }
 }
